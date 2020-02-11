b7620a4f6fb75c1a9b49e25a276553495ba14a4e
João Vitor <joaovitorcoelho13@gmail.com>
2020-02-11 15:30:27 -0300

    Add state

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
 class TechList extends Component{
+  state = {
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native'
+    ]
+  }
   render(){
+    console.log(this.state);
     return (
       <ul>
         <li>Node.js</li>
