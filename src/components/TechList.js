9a7c5304ddf92ba281f6bc1ba6dbcde11f68ede4
João Vitor <joaovitorcoelho13@gmail.com>
2020-02-11 16:24:14 -0300

    Remove from state

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,21 +2,51 @@ import React, { Component } from 'react';
 
 class TechList extends Component{
   state = {
+    newTech: '',
     techs: [
       'Node.js',
       'ReactJS',
       'React Native'
     ]
   }
-  render(){
-    console.log(this.state);
-    return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
 
+  handleInputChange = e =>{
+    this.setState({ newTech: e.target.value });
+  } 
+
+  handlSubmit = e =>{
+    e.preventDefault();
+    
+    this.setState({ 
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ''
+    });
+
+  }
+  handleDelete = (tech) =>{
+    this.setState({ techs: this.state.techs.filter( t => t!==tech)})
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.handlSubmit}>
+          <ul>
+            {this.state.techs.map(tech => (
+              <li key={tech}>
+                {tech}
+              <button 
+              onClick={() => this.handleDelete(tech)}
+              type="button">Remover</button>
+              </li>
+            ))}
+          </ul>
+        <input 
+        type="text"
+        onChange = {this.handleInputChange}
+        value = {this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
